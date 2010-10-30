595ea97d1155a4f7cd3387c1192cc458d50d8f9f
Matt Dawson <matthewtdawson@gmail.com>
2010-10-30 16:15:51 -0400

    Click event triggers name retrieval.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
 
-	$.getJSON('/couchdb/addressbook/_design/results/_view/all' , function(data) {
-		var names = [];
-		for (i = 0, max = data['rows'].length; i < max; i += 1) {
-			names[i] = data['rows'][i]['value'];
-		}
-		$.template("nameTemplate", "<li>${first_name} ${last_name}</li>");
-		$.tmpl("nameTemplate", names).appendTo("#names");
+	$.template("nameTemplate", "<li>${first_name} ${last_name}</li>");
+
+	$("#get-names").click(function () {
+		$.getJSON('/couchdb/addressbook/_design/results/_view/all', function (data) {
+			var names = [];
+			for (i = 0, max = data['rows'].length; i < max; i += 1) {
+				names[i] = data['rows'][i]['value'];
+			}
+			$("#names").html($.tmpl("nameTemplate", names));
+		});
 	});
 
 });
