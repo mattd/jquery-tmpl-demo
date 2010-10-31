618bde9d706096beaafb78b4ead2f98988fdea97
Matt Dawson <matthewtdawson@gmail.com>
2010-10-30 23:21:12 -0400

    Genericize, add documentation.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 $(document).ready(function () {
 
+	// A named template. Note that the template string could come from the
+	// database, possibly from the same document as the data.
 	$.template("nameTemplate", "<li>${first_name} ${last_name}</li>");
 
+	// On click, retrieve the values from CouchDB, render the template, and add
+	// to the DOM.
 	$("#get-names").click(function () {
 		$.getJSON('/couchdb/addressbook/_design/results/_view/all', function (data) {
-			var names = [];
-			for (i = 0, max = data['rows'].length; i < max; i += 1) {
-				names[i] = data['rows'][i]['value'];
-			}
-			$("#names").html($.tmpl("nameTemplate", names));
+			var rendered_result = $.tmpl("nameTemplate", getRowValues(data));
+			$("#names").html(rendered_result);
 		});
 	});
+
+	function getRowValues(data) {
+		// Returns the values from a CouchDB view (ie. leaves out the metadata).
+		var i = 0,
+			max,
+			values = [];
+		for (i = 0, max = data['rows'].length; i < max; i += 1) {
+			values[i] = data['rows'][i]['value'];
+		}
+		return values;
+	}
 
 });
